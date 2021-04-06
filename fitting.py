f17b752f8b4c89731fb5114edce350d236858028
Phönix 64 <52720948+Phoenix-64@users.noreply.github.com>
2021-04-06 10:53:33 +0200

    -Final

--- fitting.py
+++ fitting.py
@@ -1,28 +1,50 @@
 import numpy as np
+from datetime import date
 import pandas as pd
 import plotly.graph_objects as go
 
+"""
+Straightens curve by taking the ln of the y axis.
+"""
+
 
 def to_log(data):
-    return np.log(data)
+    data["logCases"] = np.log(data["Cases"])
+    return data
+
+
+"""
+Stretches y axis back out exponentially.
+"""
 
 
 def to_expo(data):
-    return np.power(np.e, data)
+    data["Cases"] = np.power(np.e, data["Cases"])
+    return data
 
 
-def curve_to_expo(a, b):
+"""
+Converts line formula to exponential form n1 = n * e^kx
+"""
+
+
+def line_to_expo(a, b):
     n = np.power(np.e, a)
     k = b
     return n, k
 
 
-def get_curve(data):
-    x_mean = np.mean(data.index.to_list())
-    y_mean = np.mean(data["Cases"].to_list())
+"""
+Fits curve to linear dataset:
+"""
 
-    x_intermid = (data.index.to_list()) - x_mean
-    y_intermid = data - y_mean
+
+def get_curve(data):
+    x_mean = np.mean(data["time"].to_list())
+    y_mean = np.mean(data["logCases"].to_list())
+
+    x_intermid = (data["time"].to_list()) - x_mean
+    y_intermid = data["logCases"] - y_mean
 
     x_squared = sum(x_intermid ** 2)
     x_y_comb = sum(x_intermid * y_intermid)
@@ -33,12 +55,74 @@ def get_curve(data):
     return a, b
 
 
-def plot(data):
-    fig = go.Figure(data=go.Scatter(y=data["Cases"].to_list(), x=data.index.to_list(), connectgaps=True))
-    fig.show()
+"""
+Computes days between input and last entry to data
+"""
 
-# data_base = pd.Series([1, 1.5, 2.25, 3.375])
-# data_log = to_log(data_base)
-# a, b = get_curve(data_log)
-# n, k = curve_to_expo(a, b)
-# print(n, k)
+
+def compute_delta(data, input_date):
+    prediction_date = str_to_date(input_date)
+    orig_date = str_to_date(data["Dates"].iloc[-1:].to_list()[0])
+    delta = prediction_date - orig_date
+    return delta.days
+
+
+"""
+Generates curve that fits to the dataset.
+"""
+
+
+def generate_curve(data, n, k, input_date):
+    delta = compute_delta(data, input_date)
+
+    time_range = np.arange(data["time"].iloc[-1:].to_list()[0] + 1, data["time"].iloc[-1:].to_list()[0] + delta)
+    rang = pd.DataFrame(time_range.tolist(), columns=["time"])
+    data = data.append(rang, ignore_index=True)
+    data["Interpolated"] = n * (np.e ** (k * data["time"]))
+
+    return data
+
+
+"""
+Computes the cases at input_date and if possible gets the true cases fro comparison
+"""
+
+
+def get_prediction(data, read, input_date):
+    true_cases = []
+    delta = compute_delta(data, input_date)
+    data = to_log(data)
+    a, b = get_curve(data)
+    n, k = line_to_expo(a, b)
+
+    cases = data["Cases"].iloc[-1:].to_list()[0] * (np.e ** (k * delta))
+
+    for i in read:
+        if input_date in i[0]:
+            print(i[0], "Yes")
+            true_cases = i[0].split(";")[1]
+
+    return cases, true_cases, delta
+
+
+"""
+Converts a date from form(20.01.2020) to a datetime.date object
+"""
+
+
+def str_to_date(string):
+    sepperate = string.split(".")
+    date_obj = date(int(sepperate[2]), int(sepperate[1]), int(sepperate[0]))
+    return date_obj
+
+
+"""
+Plots column in data against time
+"""
+
+
+def plot(data):
+    fig = go.Figure()
+    for column in data.columns[2:]:
+        fig.add_trace(go.Scatter(y=data[column].to_list(), x=data["time"].to_list(), connectgaps=True))
+    fig.show()
---
+++ main.py
@@ -0,0 +1,93 @@
+import csv
+import pandas as pd
+import numpy as np
+import fitting
+
+"""
+read_files reads the csv file and stores it as a list
+"""
+
+
+def read_files(filepath):
+    with open(filepath, newline="") as csvfile:
+        data = list(csv.reader(csvfile))
+    return data
+
+
+"""
+filtering takes in the raw csv file and  a list of data (20.12.2020) 
+as well as a range which it should read from the raw data. Then it separates the lines and puts them 
+into there own lists. 
+"""
+
+
+def get_cases_raw(read, dates):
+    date_ranged = []
+    current = []
+    for i in range(7, len(read)):
+        date_ranged.append(read[i])
+
+    for i in date_ranged:
+        separated = i[0].split(";")
+        for index, row in dates.iterrows():
+            if separated[0] == row[0]:
+                current.append(int(separated[1]))
+    dates["Cases"] = current
+
+    return dates
+
+
+"""
+Generates dates used for reference data
+"""
+
+
+def generate_dates(starting, length):
+    remove = []
+    dates_raw = pd.date_range(start=starting, periods=length)
+    dates_filtered = dates_raw.strftime('%d.%m.%Y')
+    date_series = pd.DataFrame({"Dates": dates_filtered})
+
+    for i in dates_raw:
+        if i.dayofweek == 5 or i.dayofweek == 6:
+            remove.append(np.where(dates_raw == i)[0].item())
+    date_series = date_series.drop(remove)
+    date_series["time"] = date_series.index.to_list()
+    return date_series
+
+
+# Read data from file and get curve parameters
+read = read_files("200325_Datengrundlage_Grafiken_COVID-19-Bericht.csv")
+
+# Generates dates which are used to generate the line
+data = generate_dates("15-02-2021", 43)
+
+# Gets cases for these dates and fits curve
+data = get_cases_raw(read, data)
+data = fitting.to_log(data)
+a, b = fitting.get_curve(data)
+n, k = fitting.curve_to_expo(a, b)
+print(a, b)
+print(n, k)
+
+# Get The Prediction for the following dates:
+prediction_dates = ["27.04.2021"]
+case_comp = pd.DataFrame()
+pred_cases = []
+actual_cases = []
+delta = []
+
+for i in prediction_dates:
+    pred, actual, delta_time = fitting.get_prediction(data, read, i)
+    pred_cases.append(pred)
+    actual_cases.append(actual)
+    delta.append(delta_time)
+
+case_comp.insert(0, "preCases", pred_cases)
+case_comp.insert(1, "actCases", actual_cases)
+case_comp.insert(2, "DeltaTime", delta)
+
+# Generates and plots curve as well as print prediction:
+print(case_comp)
+data = fitting.generate_curve(data, n, k, prediction_dates[0])
+fitting.plot(data)
